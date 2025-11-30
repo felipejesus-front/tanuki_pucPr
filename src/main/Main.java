93df3a2e2f9a33312125e78122e76673145e6d7e
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-11-30 19:24:39 -0300

    Deixando a função que cria financiamento mais abstrado, de modo que receba qualquer classe que extenda Financiamento

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import modelo.Casa;
 import modelo.Financiamento;
 import util.InterfaceUsuario;
 
@@ -14,8 +15,8 @@ public class Main {
     public static void main(String[] args) {
 
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
-        List<Financiamento> financiamentoList = criarFinanciamentos(interfaceUsuario, 4);
-
+        List<? extends Financiamento> financiamentoList = criarFinanciamentos(interfaceUsuario, 2, Financiamento.class);
+        List<? extends Financiamento> financiamentoCasa = criarFinanciamentos(interfaceUsuario, 2, Casa.class);
         for (Financiamento financiamento : financiamentoList) {
             financiamento.mostrarDadosFinanciamento();
         }
@@ -29,7 +30,7 @@ public class Main {
 
     //Função para criar financiamentos diferentes com opção de quantidade de financiamentos.
     //por enquanto vai ser hardCoded e não por terminal. mas pedir a quantidade pro cliente pode ser util
-    public static List<Financiamento> criarFinanciamentos(InterfaceUsuario interfaceUsuario, int quantidade) {
+    public static List<? extends Financiamento> criarFinanciamentos(InterfaceUsuario interfaceUsuario, int quantidade, Class<? extends Financiamento> tipoFinanciamento) {
         List<Financiamento> financiamentoList = new ArrayList<>();
         for (int i = 0; i < quantidade; i++) {
             System.out.println("Financiamento #" + (i + 1));
@@ -37,13 +38,20 @@ public class Main {
             int prazoFinanciamentoEmAnos = interfaceUsuario.pedirPrazoFinanciamento();
             double taxaJuros = interfaceUsuario.pedirTaxaDeJuros();
 
-            financiamentoList.add(new Financiamento(valorImovel, prazoFinanciamentoEmAnos, taxaJuros));
+            try {
+                Financiamento financiamento = tipoFinanciamento
+                        .getConstructor(double.class, int.class, double.class)
+                        .newInstance(valorImovel, prazoFinanciamentoEmAnos, taxaJuros);
+                financiamentoList.add(financiamento);
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao criar financiamento", e);
+            }
         }
         return financiamentoList;
     }
 
     //função para mostrar total que recebe o campo e o texto com o nome do campo.
-    public static void mostrarTotal(List<Financiamento> lista, String nomeCampo, java.util.function.ToDoubleFunction<Financiamento> extractor) {
+    public static void mostrarTotal(List<? extends Financiamento> lista, String nomeCampo, java.util.function.ToDoubleFunction<Financiamento> extractor) {
         double total = lista.stream().mapToDouble(extractor).sum();
         System.out.println("Valor total de " + nomeCampo + ": R$ " + String.format("%,.2f", total));
     }
