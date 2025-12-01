963e6c5d9afe2bab78732b4eabb635530c2110b9
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-12-01 14:28:06 -0300

    adicionando calculo de pagamento mensal de terreno

--- src/modelo/Terreno.java
+++ src/modelo/Terreno.java
@@ -6,4 +6,9 @@ public class Terreno  extends Financiamento{
     public Terreno(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
         super(valorImovel, prazoFinanciamento, taxaJurosAnual);
     }
+
+    public double calcularPagamentoMensal(){
+        double doisPorcento = 0.02;
+        return super.calcularPagamentoMensal() + (super.calcularPagamentoMensal() * doisPorcento) ;
+    }
 }
