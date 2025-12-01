4ec849a6671a616caf3faeb4477a43db39edb0ea
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-12-01 14:32:53 -0300

    Adicionando calculo prime ao calculo mensal do apartamento

--- src/modelo/Apartamento.java
+++ src/modelo/Apartamento.java
@@ -4,4 +4,14 @@ public class Apartamento extends Financiamento {
     public Apartamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
         super(valorImovel, prazoFinanciamento, taxaJurosAnual);
     }
+
+    public double calcularPagamentoMensal() {
+        double taxaMensal = taxaJurosMensal(getTaxaJurosAnual());
+        int meses = prazoFinanciamentoEmMeses();
+        double base = 1 + taxaMensal;
+        double potencia = Math.pow(base, meses);
+        double numerador = getValorImovel() * taxaMensal * potencia;
+        double denominador = potencia - 1;
+        return numerador / denominador;
+    }
 }
