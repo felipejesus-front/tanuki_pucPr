b697db7d6d68ef5843077572e7c2b0e98465dc0a
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-12-01 14:32:19 -0300

    formatando espaço extra

--- src/modelo/Apartamento.java
+++ src/modelo/Apartamento.java
@@ -1,6 +1,6 @@
 package modelo;
 
-public class Apartamento extends Financiamento{
+public class Apartamento extends Financiamento {
     public Apartamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
         super(valorImovel, prazoFinanciamento, taxaJurosAnual);
     }
