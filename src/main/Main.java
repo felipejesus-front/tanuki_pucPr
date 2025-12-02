2bd521475bb8a154a95308af7145fbe38f2d0f5b
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-12-01 22:28:15 -0300

    refatorando criarFinanciamentos para cadastrar campos dependendo do tipo de financiamento

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,37 +19,71 @@ public class Main {
 
         //crio as listas de financiamentos de qualquer um dos tipos de financiamentos
         criarFinanciamentos(interfaceUsuario, 2, "Casa", Casa.class);
-
-        criarFinanciamentos(interfaceUsuario, 2,"Apartamento", Apartamento.class);
-
+        criarFinanciamentos(interfaceUsuario, 2, "Apartamento", Apartamento.class);
         criarFinanciamentos(interfaceUsuario, 2, "Terreno", Terreno.class);
 
     }
 
-    //Fiz uma mudança nesse método para receber também classes que extendem financiamento. Não sabia que dava para fazer isso, obrigado pelo incentivo!
-    public static void criarFinanciamentos(InterfaceUsuario interfaceUsuario, int quantidade, String nomeFinanciamento, Class<? extends Financiamento> tipoFinanciamento) {
+    public static List<? extends Financiamento> criarFinanciamentos(
+            InterfaceUsuario interfaceUsuario,
+            int quantidade,
+            String nomeFinanciamento,
+            Class<? extends Financiamento> tipoFinanciamento) {
+
         List<Financiamento> financiamentoList = new ArrayList<>();
+
+        System.out.println("\n=== Criando Financiamentos de " + nomeFinanciamento + " ===\n");
+
         for (int i = 0; i < quantidade; i++) {
-            System.out.println("Financiamento  de "+ nomeFinanciamento + " " + (i + 1));
+            System.out.println("Financiamento de " + nomeFinanciamento + " " + (i + 1));
 
             try {
-                Financiamento financiamentoTemp = tipoFinanciamento.getConstructor(double.class, int.class, double.class).newInstance(0.0, 1, 0.0);
+                // Cria instância temporária para coletar dados básicos
+                Financiamento financiamentoTemp = tipoFinanciamento
+                        .getConstructor(double.class, int.class, double.class)
+                        .newInstance(0.0, 1, 0.0);
 
+                // Coleta dados básicos (comuns a todos os financiamentos)
                 double valorImovel = interfaceUsuario.pedirValorImovel(financiamentoTemp);
                 int prazoFinanciamentoEmAnos = interfaceUsuario.pedirPrazoFinanciamento(financiamentoTemp);
                 double taxaJuros = interfaceUsuario.pedirTaxaDeJuros(financiamentoTemp);
 
+                // Switch para coletar dados específicos e criar instância final
+                Financiamento financiamento;
+                switch (nomeFinanciamento) {
+                    case "Casa":
+                        Casa casaTemp = (Casa) financiamentoTemp;
+                        double tamanhoareaConstrucao = interfaceUsuario.pedirTamanhoareaConstrucao(casaTemp);
+                        double tamanhoTerreno = interfaceUsuario.pedirTamanhoTerreno(casaTemp);
+                        financiamento = new Casa(valorImovel, prazoFinanciamentoEmAnos, taxaJuros, tamanhoareaConstrucao, tamanhoTerreno);
+                        break;
 
-                Financiamento financiamento = tipoFinanciamento
-                        .getConstructor(double.class, int.class, double.class)
-                        .newInstance(valorImovel, prazoFinanciamentoEmAnos, taxaJuros);
+                    case "Apartamento":
+                        Apartamento apartamentoTemp = (Apartamento) financiamentoTemp;
+                        int numeroVagasGaragem = interfaceUsuario.pedirNumeroVagasGaragem(apartamentoTemp);
+                        int numeroAndar = interfaceUsuario.pedirNumeroAndar(apartamentoTemp);
+                        financiamento = new Apartamento(valorImovel, prazoFinanciamentoEmAnos, taxaJuros, numeroVagasGaragem, numeroAndar);
+                        break;
+
+                    case "Terreno":
+                        Terreno terrenoTemp = (Terreno) financiamentoTemp;
+                        String tipoZona = interfaceUsuario.pedirTipoZona(terrenoTemp);
+                        financiamento = new Terreno(valorImovel, prazoFinanciamentoEmAnos, taxaJuros, tipoZona);
+                        break;
+
+                    default:
+                        throw new IllegalArgumentException("Tipo de financiamento desconhecido: " + nomeFinanciamento);
+                }
+
                 financiamentoList.add(financiamento);
+
             } catch (Exception e) {
                 throw new RuntimeException("Erro ao criar financiamento de " + nomeFinanciamento, e);
             }
         }
 
         mostrarResumoFinanciamentos(financiamentoList, interfaceUsuario, nomeFinanciamento);
+        return financiamentoList;
     }
 
     //função para mostrar total que recebe o campo e o texto com o nome do campo.
@@ -58,7 +92,6 @@ public class Main {
         System.out.println("Valor total de " + nomeCampo + ": R$ " + String.format("%,.2f", total));
     }
 
-    //Se der certo, esse método vai mostrar qualquer tipo de financiamento q eu quiser, seja casa, Apartamento ou Terreno, desde que extenda Financiamento.
     public static void mostrarResumoFinanciamentos(
             List<? extends Financiamento> lista,
             InterfaceUsuario interfaceUsuario,
